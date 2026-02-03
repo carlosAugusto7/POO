9147726d1b2136beb711f2e7f35e2f361359c570
Carlos Augusto <carlosaugusto20003@gmail.com>
2026-02-02 21:23:44 -0300

    Add Programa06 to handle file input exceptions
    Tem que criar o arquivo na máquina em que estiver rodando e ver a magia acontecer

---
+++ Exececoes/controle/Programa06.java
@@ -0,0 +1,21 @@
+package controle;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class Programa06 {
+
+	public static void main(String[] args) {
+		String caminho = "C:/Users/aluno.unilasalle/Documents/arquivo.txt";
+		
+		try {
+			FileInputStream arquivo = new FileInputStream(caminho);
+			System.out.println("Arquivo existe no disco");
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("Arquivo não existe no disco");
+			System.out.println("Erro: " + e.getMessage());
+		}
+	}
+
+}
