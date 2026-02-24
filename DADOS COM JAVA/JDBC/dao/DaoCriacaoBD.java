e5e8bcf464e103669ba2f8d6ed01d5d58761810e
Carlos Augusto <carlosaugusto20003@gmail.com>
2026-02-23 21:10:17 -0300

    Create DaoCriacaoBD.java

---
+++ DADOS COM JAVA/JDBC/dao/DaoCriacaoBD.java
@@ -0,0 +1,30 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DaoCriacaoBD {
+
+	static final String url = "jdbc:mysql://localhost";
+	
+	public DaoCriacaoBD() {
+		String sql = "CREATE DATABASE mgu";
+		
+		try {
+			Connection conexao = DriverManager.getConnection(url, "root" ,"");
+			
+			PreparedStatement operacao = conexao.prepareStatement(sql);
+			operacao.execute();
+			
+			System.out.println("Banco de dados criado com sucesso !");
+			conexao.close();
+			
+		}
+		catch (SQLException e){
+			System.out.println("Erro: " + e.getMessage());
+		}
+	}
+  }
+ 
