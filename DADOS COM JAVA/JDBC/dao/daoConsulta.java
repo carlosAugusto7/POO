c02e81040d3c23446e29177e244f3a0c7d096aff
Carlos Augusto <carlosaugusto20003@gmail.com>
2026-02-23 21:31:58 -0300

    Create DaoConsulta class for database queries
    Implement DaoConsulta class to fetch game data from database

---
+++ DADOS COM JAVA/JDBC/dao/daoConsulta.java
@@ -0,0 +1,33 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DaoConsulta {
+
+	static final String url = "jdbc:mysql://localhost/mgu";
+	
+	public DaoConsulta(){
+		String sql = "SELECT * FROM game WHERE id = 3";
+		
+		try {
+			Connection conexao = DriverManager.getConnection(url, "root" ,"");
+			
+			PreparedStatement operacao = conexao.prepareStatement(sql);
+			ResultSet resultado = operacao.executeQuery();
+			
+			resultado.first();
+			System.out.println(resultado.getObject(1) + " | " + resultado.getObject(2) + " (" + resultado.getObject(3) + ")");
+			
+			conexao.close();
+			
+		}
+		catch (SQLException e){
+			System.out.println("Erro: " + e.getMessage());
+			
+		}
+	}	
+}
