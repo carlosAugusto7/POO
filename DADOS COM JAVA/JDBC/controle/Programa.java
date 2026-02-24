1f217e42cca6ae4ceec498be5e45d9308fb6e5bc
Carlos Augusto <carlosaugusto20003@gmail.com>
2026-02-23 21:07:50 -0300

    Create Programa class with main method
    Initial implementation of the Programa class with main method.

---
+++ DADOS COM JAVA/JDBC/controle/Programa.java
@@ -0,0 +1,18 @@
+package controle;
+
+import dao.*;
+
+public class Programa {
+
+	public static void main(String[] args) {
+	// DaoConexao daoConexao = new DaoConexao();
+	//DaoCriacaoBD daoCriacaoBd = new DaoCriacaoBD();
+	//DaoCriacaoTabela daoCriacaoTabela = new DaoCriacaoTabela();
+	//DaoInclusao1 daoInclusao1 = new DaoInclusao1();
+	//DaoInclusao2 daoInclusao2= new DaoInclusao2();
+	//DaoAlteracao daoAlteracao = new DaoAlteracao();
+	//DaoExcluir daoExcluir = new DaoExcluir();
+	//DaoLista daoLista = new DaoLista();
+	}
+
+}
