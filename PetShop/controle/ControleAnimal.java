2bcc07b6604f438a2f5aa44dadc3325f9ec4aed8
Carlos Augusto <carlosaugusto20003@gmail.com>
2026-03-02 19:29:37 -0300

    Add ControleAnimal class for animal management

---
+++ PetShop/controle/ControleAnimal.java
@@ -0,0 +1,25 @@
+package controle;
+
+import modelo.Animal;
+
+public class ControleAnimal {
+
+	private Animal animal = new Animal();
+	
+	public void cadastrarAnimal() {
+		
+	}
+	
+	public void consultarAnimal() {
+		
+	}
+	
+	public void alternarAnimal() {
+		
+	}
+	
+	public void excluirAnimal() {
+		
+	}
+	
+}
