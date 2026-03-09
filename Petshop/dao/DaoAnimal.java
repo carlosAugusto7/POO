e0c385a858fb5d04d2c18fedd184caf9c82269dd
Carlos Augusto <carlosaugusto20003@gmail.com>
2026-03-09 20:53:37 -0300

    Update DaoAnimal.java

--- Petshop/dao/DaoAnimal.java
+++ Petshop/dao/DaoAnimal.java
@@ -1,14 +1,55 @@
 package dao;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import modelo.Animal;
 
 public class DaoAnimal {
-	private static final String url = "jdbc:mysql://localhost/petshop";
-	
 	public void inserir(Animal animal) {
-		String sql = "INSERT INTO animal (nome, especie, raca) VALUES ('" + animal.getNome() + "', '" + animal.getEspecie() + "', '" + animal.getRaca() + "')";
-	
+		String sql = "INSERT INTO animal (nome, especie, raca) VALUES (?,?,?)";	
 		System.out.println(sql);
+		
+		try {
+			Properties props = new Properties();
+			FileInputStream arquivo = new  FileInputStream("db.properties");
+			props.load(arquivo);
+			
+			String driver = props.getProperty("db.driver");
+			String url = props.getProperty("db.url");
+			String usuario = props.getProperty("db.usuario");
+			String senha = props.getProperty("db.senha");
+			
+			Class.forName(driver);
+			Connection conexao = DriverManager.getConnection(url, usuario, senha);
+			PreparedStatement operacao = conexao.prepareStatement(sql);
+			operacao.setString(1, animal.getNome());
+			operacao.setString(2, animal.getEspecie());
+			operacao.setString(3, animal.getRaca());
+			operacao.execute();
+			
+			System.out.println("Animal cadastrado com sucesso !!");
+			conexao.close();
+			operacao.close();
+		}
+		catch(SQLException e){
+			System.out.println("ERRO: " + e.getMessage());
+		}
+		catch(FileNotFoundException e) {
+			System.out.println("ERRO: " + e.getMessage());
+		}
+		catch(IOException e) {
+			System.out.println("ERRO: " + e.getMessage());
+		}
+		catch (ClassNotFoundException e ) {
+			System.out.println("ERRO: " + e.getMessage());
+		}
 	}
 	
 	public void consultar() {
